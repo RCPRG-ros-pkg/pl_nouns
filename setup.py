aa5b94fdb2e6014f9cff6076186898ee991d288b
Adam Krawczyk <adam.krawczyk@robotec.ai>
2024-01-09 20:04:12 +0100

    Update to fix build issue
    Signed-off-by: Adam Krawczyk <adam.krawczyk@robotec.ai>

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-#!/usr/bin/env python3
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'pl_nouns'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/data', ['data/rzecz_pol_compact.txt']),
-        # You can include additional data files here as needed
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='TODO',
-    maintainer_email='5305@pw.edu.pl',
-    description='The pl_nouns package',
-    license='TODO', # Update the license as appropriate
+    maintainer='adam',
+    maintainer_email='adam.krawczyk@robotec.ai',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # Here you can add your Python executables
-            # For example: 'executable_name = package_name.module_name:main'
+            'dictionary_service = pl_nouns.dictionary_service:main',
+            'dictionary_client = pl_nouns.dictionary_client:main',
+            'dictionary_test = pl_nouns.dictionary_test:main'
         ],
     },
 )
